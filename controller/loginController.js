9485aaa3181f57bda0b06fe0f3ffc8b6c16d15b4
park <10330697048@qq.com>
2017-07-18 17:58:13 +0800

    'register'

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,7 +1,9 @@
+'use strict';
 /**
 	user login/logout
 */
-
+import userModel from '../models/userModel.js';
+import formidable from 'formidable';
 class loginController{
 	constructor(){
 
@@ -13,9 +15,27 @@ class loginController{
 		})
 	}
 	async register(req, res, next){
-		res.send({
-			code:200,
-			data:'success'
+		let form = new formidable.IncomingForm();
+		form.parse(req,(error,fields, files)=>{
+			if(error){
+				res.send({
+					code:-200,
+					type:'FROM_DATA_ERROR',
+					message:'表单内容有误'
+				})
+				return;
+			}
+			let {username,password} = fields;
+			await let admin = userModle.findOne({username})
+			if(admin){
+				res.send({
+					code:-200,
+					type:'USER_DATA_ERROR',
+					message:'用户民已经存在'
+				})
+				return;
+			}
+
 		})
 	}
 	async logout(req, res, next){
